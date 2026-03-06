836d934043117ddc50a60cc6f836c133b693b98d
LucianaDornaicka <dornaicka.lu@gmail.com>
2026-03-06 12:40:01 -0300

    Update twilio.js

--- src/services/twilio.js
+++ src/services/twilio.js
@@ -1,14 +1,20 @@
 import twilio from 'twilio';
 
-const client = twilio(
-  process.env.TWILIO_ACCOUNT_SID,
-  process.env.TWILIO_AUTH_TOKEN
- );
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const messagingServiceSid = "MG6a603db341a48ae6386662540bc37603";
+
+const client = twilio(accountSid, authToken);
 
 export async function sendMessage(to, body) {
-  return client.messages.create({
-    from: process.env.TWILIO_WHATSAPP_FROM,
-    to,
-    body,
-  });
+  try {
+    await client.messages.create({
+      messagingServiceSid: messagingServiceSid,
+      to: to,
+      body: body,
+    });
+    console.log(`Mensagem enviada para ${to}`);
+  } catch (error) {
+    console.error("Erro ao enviar mensagem pelo Twilio:", error.message);
+  }
 }
